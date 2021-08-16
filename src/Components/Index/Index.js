6a0f16ee8c2a85ee72f643937c5f4a9f6f428270
Marcus Pontes <marcusporama@gmail.com>
2021-08-16 00:20:52 -0300

    criado arquivo api e feito fetch no arquivo index

--- src/Components/Index/Index.js
+++ src/Components/Index/Index.js
@@ -1,8 +1,36 @@
-import { Form } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import { Form, Table, Button, Modal } from "react-bootstrap"
 
-import { FullContainer, TextoFirst, Busca } from "./Styles"
+import {
+  FullContainer,
+  TextoFirst,
+  Busca,
+  FullTabela,
+  ButtonAction,
+} from "./Styles"
+
+import api from "../api/api"
 
 function Index() {
+  const [dados, setDados] = useState([])
+  const [page, setPage] = useState([])
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  useEffect(() => {
+    getDados()
+  }, [])
+
+  async function getDados() {
+    const response = await api.get("?results=50")
+    const docs = response.data
+
+    setDados(docs)
+    console.log(dados.results)
+  }
+
   return (
     <FullContainer>
       <TextoFirst>
@@ -15,6 +43,49 @@ function Index() {
           <Form.Control type="text" placeholder="Searching" />
         </Form>
       </Busca>
+      <FullTabela>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>Birth</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Marcus</td>
+              <td>Masc</td>
+              <td>28</td>
+              <td>
+                <ButtonAction>
+                  <Button variant="outline-primary" onClick={handleShow}>
+                    Show
+                  </Button>
+
+                  <Modal show={show} onHide={handleClose} animation={false}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      Woohoo, you're reading this text in a modal!
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </ButtonAction>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </FullTabela>
     </FullContainer>
   )
 }
--- src/Components/Index/Styles.js
+++ src/Components/Index/Styles.js
@@ -3,14 +3,23 @@ import styled from "styled-components"
 export const FullContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
+  width: 70%;
   align-items: center;
   justify-content: center;
-  margin-left: 30%;
+  margin-left: 13%;
 `
 export const TextoFirst = styled.div``
 
 export const Busca = styled.div`
   margin-top: 20px;
+  width: 100%;
+`
+
+export const FullTabela = styled(Busca)``
+
+export const ButtonAction = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
 `
