d8d6b7f23b8a6f6338fb9744b14ae4ac588249f5
Marcus Pontes <marcusporama@gmail.com>
2021-08-15 23:17:42 -0300

    instalção do styled component e estiliazação do index.js

--- src/Components/Index/Index.js
+++ src/Components/Index/Index.js
@@ -1,5 +1,22 @@
+import { Form } from "react-bootstrap"
+
+import { FullContainer, TextoFirst, Busca } from "./Styles"
+
 function Index() {
-  return <h1>Index</h1>
+  return (
+    <FullContainer>
+      <TextoFirst>
+        Olpus igitur est dicere possit dura omni specie. "Tu autem in specie,
+        non videntur, necomnino res est. " Et examine ab eis praecepta eius quae
+        habes, el prime et principaliter
+      </TextoFirst>
+      <Busca>
+        <Form>
+          <Form.Control type="text" placeholder="Searching" />
+        </Form>
+      </Busca>
+    </FullContainer>
+  )
 }
 
 export default Index
---
+++ src/Components/Index/Styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const FullContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  align-items: center;
+  justify-content: center;
+  margin-left: 30%;
+`
+export const TextoFirst = styled.div``
+
+export const Busca = styled.div`
+  margin-top: 20px;
+  width: 100%;
+`
