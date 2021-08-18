3d25bf1f1a10900316f7aa8b5bf36ad126c7ae2f
Marcus Pontes <marcusporama@gmail.com>
2021-08-18 11:39:30 -0300

    search implementado

--- src/Components/Index/Index.js
+++ src/Components/Index/Index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Form, Table, Button, Modal, Image } from "react-bootstrap"
+import { Form, Table, Button, Modal, Image, Spinner } from "react-bootstrap"
 
 import {
   FullContainer,
@@ -15,9 +15,11 @@ import api from "../api/api"
 
 function Index() {
   const [dados, setDados] = useState([])
+  const [loading, setLoading] = useState(true)
   const [newDados, setNewDados] = useState({})
   const [page, setPage] = useState([])
   const [show, setShow] = useState(false)
+  const [search, setSearch] = useState("")
 
   const handleClose = () => setShow(false)
 
@@ -26,11 +28,12 @@ function Index() {
   }, [])
 
   async function getDados() {
-    const response = await api.get("?results=50")
+    const response = await api.get("?results=50&sort_by")
     const docs = response.data
 
     setPage(docs.info.page)
     setDados(docs.results)
+    setLoading(false)
   }
   async function handleShow(id) {
     const response = await api.get(`?id=${id}`)
@@ -56,60 +59,76 @@ function Index() {
     setPage(docs.info.page)
   }
 
+  const filteredNames = dados.filter((names) => {
+    return names.name.first.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <FullContainer>
-      <TextoFirst>
-        Olpus igitur est dicere possit dura omni specie. "Tu autem in specie,
-        non videntur, necomnino res est. " Et examine ab eis praecepta eius quae
-        habes, el prime et principaliter
-      </TextoFirst>
-      <Busca>
-        <Form>
-          <Form.Control type="text" placeholder="Searching" />
-        </Form>
-      </Busca>
-      <FullTabela>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Birth</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
+      {loading ? (
+        <Spinner animation="grow" variant="primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <div>
+          <TextoFirst>
+            Olpus igitur est dicere possit dura omni specie. "Tu autem in
+            specie, non videntur, necomnino res est. " Et examine ab eis
+            praecepta eius quae habes, el prime et principaliter
+          </TextoFirst>
+          <Busca>
+            <Form>
+              <Form.Control
+                type="text"
+                placeholder="Searching"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              {search}
+            </Form>
+          </Busca>
+          <FullTabela>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Gender</th>
+                  <th>Birth</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
 
-          <tbody>
-            {dados.map((dados) => (
-              <tr key={dados.login.uuid}>
-                <>
-                  <td>{dados.name.first}</td>
-                  <td>{dados.gender}</td>
-                  <td>{dados.dob.age}</td>
+              <tbody>
+                {filteredNames.map((dados) => (
+                  <tr key={dados.login.uuid}>
+                    <>
+                      <td>{dados.name.first}</td>
+                      <td>{dados.gender}</td>
+                      <td>{dados.dob.age}</td>
 
-                  <td>
-                    <ButtonAction>
-                      <Button
-                        variant="outline-primary"
-                        onClick={() => {
-                          handleShow(dados.id.value)
-                        }}
-                      >
-                        Show
-                      </Button>
-                    </ButtonAction>
-                  </td>
-                </>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </FullTabela>
-      <Pagination>
-        <button onClick={previousPage}>Página Anterior</button>
-        <button onClick={nextPage}>Próxima página</button>
-        Página atual: {page}
-      </Pagination>
+                      <td>
+                        <ButtonAction>
+                          <Button
+                            variant="outline-primary"
+                            onClick={() => {
+                              handleShow(dados.id.value)
+                            }}
+                          >
+                            Show
+                          </Button>
+                        </ButtonAction>
+                      </td>
+                    </>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </FullTabela>
+          <Pagination>
+            <Button onClick={previousPage}>Página Anterior</Button>
+            <Button onClick={nextPage}>Próxima página</Button>
+          </Pagination>
+        </div>
+      )}
     </FullContainer>
   )
 }
